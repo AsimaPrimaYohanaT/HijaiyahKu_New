eba0fb2c8790021ddda935029edc9611ddbac806
wgnalvian <wgnalvian@gmail.com>
2023-09-18 08:44:13 +0700

    add: add layout in choose quest

---
+++ app/src/main/java/com/example/hijaiyahku_new/ChooseQuest.kt
@@ -0,0 +1,11 @@
+package com.example.hijaiyahku_new
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseQuest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_quest)
+    }
+}
