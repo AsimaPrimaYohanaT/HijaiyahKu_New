e0b0fef46eb142cc0463716ac9d800731df9bad1
wgnalvian <wgnalvian@gmail.com>
2023-09-19 10:34:29 +0700

    create: file logic fragment

---
+++ app/src/main/java/com/example/hijaiyahku_new/fragment/SuccessFragment.kt
@@ -0,0 +1,7 @@
+package com.example.hijaiyahku_new.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.hijaiyahku_new.R
+
+class SuccessFragment : Fragment(R.layout.custom_dialog_2) {
+}
