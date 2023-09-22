de858d863fca047cdf026b5db2a9e98da95b3de9
wgnalvian <wgnalvian@gmail.com>
2023-09-22 09:13:54 +0700

    feat: take image from gallery

--- app/src/main/java/com/example/hijaiyahku_new/DetailQuest.kt
+++ app/src/main/java/com/example/hijaiyahku_new/DetailQuest.kt
@@ -1,13 +1,34 @@
 package com.example.hijaiyahku_new
 
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.hijaiyahku_new.databinding.ActivityDetailQuestBinding
 import com.example.hijaiyahku_new.databinding.ActivityMainBinding
 import com.example.hijaiyahku_new.fragment.HintFragment
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class DetailQuest : AppCompatActivity() {
     lateinit var binding: ActivityDetailQuestBinding
+    private val FILENAME_FORMAT = "dd-MMM-yyyy"
+    private val MAXIMAL_SIZE = 1000000
+
+    val timeStamp: String = SimpleDateFormat(
+        FILENAME_FORMAT,
+        Locale.US
+    ).format(System.currentTimeMillis())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityDetailQuestBinding.inflate(layoutInflater)
@@ -18,5 +39,63 @@ class DetailQuest : AppCompatActivity() {
 //        val fragmentTransaction = fragmentManager.beginTransaction()
 //        fragmentTransaction.replace(R.id.fragmentContainer,HintFragment())
 //        fragmentTransaction.commit()
+
+        binding.galery.setOnClickListener {
+            startGallery()
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@DetailQuest)
+               binding.imageView2.setImageBitmap(uriToBitmap(applicationContext,selectedImg))
+            }
+        }
+    }
+
+
+    fun uriToBitmap(context: Context, uri: Uri): Bitmap? {
+        var inputStream: InputStream? = null
+        try {
+            inputStream = context.contentResolver.openInputStream(uri)
+            if (inputStream != null) {
+                return BitmapFactory.decodeStream(inputStream)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            inputStream?.close()
+        }
+        return null
+    }
+    fun uriToFile(selectedImg: Uri, context: Context): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val myFile = createCustomTempFile(context)
+
+        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+        val outputStream: OutputStream = FileOutputStream(myFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+        return myFile
+    }
+
+    fun createCustomTempFile(context: Context): File {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(timeStamp, ".jpg", storageDir)
     }
 }
