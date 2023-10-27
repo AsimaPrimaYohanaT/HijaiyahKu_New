0264a00a34314223191f91db9ad29008e3912eef
wgnalvian <wgnalvian@gmail.com>
2023-10-27 10:08:34 +0700

    resolve: bgm error

--- app/src/main/java/com/example/hijaiyahku_new/MainActivity.kt
+++ app/src/main/java/com/example/hijaiyahku_new/MainActivity.kt
@@ -19,29 +19,38 @@ class MainActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-//        PlayBackgroundSound(null)
-//        val animator = ObjectAnimator.ofFloat(findViewById(R.id.btn_home), "scaleX", 1f, 3f)
-//        val animator1 = ObjectAnimator.ofFloat(findViewById(R.id.btn_home), "scaleY", 1f, 3f)
-//        animator.repeatCount = ObjectAnimator.INFINITE
-//        animator.repeatMode = ObjectAnimator.REVERSE
-//        animator.duration = 1000L
-//        animator.start()
-//        animator1.repeatCount = ObjectAnimator.INFINITE
-//        animator1.repeatMode = ObjectAnimator.REVERSE
-//        animator1.duration = 1000L
-//        animator1.start()
+        PlayBackgroundSound(null)
+        playAnimation()
         binding.btnHome.setOnClickListener {
             val mulai = Intent(this@MainActivity, ChooseQuest::class.java)
             startActivity(mulai)
         }
 
     }
+
+    fun playAnimation(){
+
+        val animator = ObjectAnimator.ofFloat(findViewById(R.id.btn_home), "scaleX", 1f, 1.1f)
+        val animator1 = ObjectAnimator.ofFloat(findViewById(R.id.btn_home), "scaleY", 1f, 1.1f)
+        animator.repeatCount = ObjectAnimator.INFINITE
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.duration = 1000L
+        animator.start()
+        animator1.repeatCount = ObjectAnimator.INFINITE
+
+        animator1.repeatMode = ObjectAnimator.REVERSE
+        animator1.duration = 1000L
+        animator1.start()
+    }
     fun PlayBackgroundSound(view: View?) {
-        val intent = Intent(this@MainActivity, BackgroundSoundService::class.java)
-        startService(intent)
+        val backgroundThread = Thread {
+            val intent = Intent(this@MainActivity, BackgroundSoundService::class.java)
+            startService(intent)
+        }
+        backgroundThread.start()
     }
 
 }
